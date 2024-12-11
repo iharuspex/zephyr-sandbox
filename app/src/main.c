1172ff9969b0d193032b089f701b970b2d8a0b49
Artem Paraev <artemparaev.dev@gmail.com>
2024-12-12 02:33:03 +0700

    add pwm config, bbc_microbit overlay; update launch.json

--- app/src/main.c
+++ app/src/main.c
@@ -4,22 +4,41 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/device.h>
+
 #define DELAY_MS 300
 
-#define LED0_NODE DT_ALIAS(led0)
+#define PERIOD_MAX PWM_USEC(3900)
+#define BEEP_DURATION  K_MSEC(60)
 
+#define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static const struct pwm_dt_spec pwm = PWM_DT_SPEC_GET(DT_PATH(zephyr_user));
 
 int main(void)
 {
 	int ret;
 	bool led_state = true;
 
+	uint32_t period = PWM_USEC(2000);
+
 	printk("Zephyr Sandbox App\n");
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
 	}
+
+	if (!pwm_is_ready_dt(&pwm)) {
+		printk("%s: device is not ready\n", pwm.dev->name);
+		return 0;
+	}
+
+	pwm_set_dt(&pwm, period, period / 2U);
+	k_sleep(BEEP_DURATION);
+	pwm_set_pulse_dt(&pwm, 0);
+	k_sleep(K_MSEC(50));
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
