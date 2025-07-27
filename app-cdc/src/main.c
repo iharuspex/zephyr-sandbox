e3ff947e5180687e830b9f3a6bd7f20a4c9f5e7e
Artem Paraev <nabrosok-hd@yandex.ru>
2025-07-27 16:28:51 +0700

    new: add app-cdc template

---
+++ app-cdc/src/main.c
@@ -0,0 +1,12 @@
+#include "zephyr/device.h"
+#include <stdlib.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/sys/printk.h>
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
