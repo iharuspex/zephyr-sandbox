6602b61a6c43beae4b67e6f102ed0c55059016e6
Artem Paraev <nabrosok-hd@yandex.ru>
2026-02-03 19:22:33 +0700

    wip: rocket_flight_logger board cleanup

--- boards/custom/rocket_flight_logger/board.cmake
+++ boards/custom/rocket_flight_logger/board.cmake
@@ -1,6 +1,3 @@
-# Copyright (c) 2021 Nordic Semiconductor ASA
-# SPDX-License-Identifier: Apache-2.0
-
 board_runner_args(jlink "--device=STM32F103C8" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
